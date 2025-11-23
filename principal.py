a44fa8002a4dee10d1fcaa8f557ef76052235dc9
Gabriel-Kwasinski <111901953+Gabriel-Kwasinski@users.noreply.github.com>
2025-11-23 19:30:35 -0300

    Updates principal.py, removing test functions for fornecedor.py

--- principal.py
+++ principal.py
@@ -5,20 +5,9 @@ import compra
 import cliente
 import validacao
 
-def testa_fornecedor():
-    fornecedor.testa_carregar_fornecedores()
-    fornecedor.testa_salvar_fornecedores()
-    fornecedor.testa_cadastrar_fornecedor()
-    fornecedor.testa_listar_fornecedores()
-    fornecedor.testa_associar_produto_fornecedor()
-    fornecedor.testa_listar_produtos_por_fornecedor()
-    fornecedor.testa_buscar_fornecedores_do_produto()
-    fornecedor.testa_contatar_fornecedor()
-    fornecedor.testa_gerar_relatorio_fornecedores()
-
 
 #Testes automaticos
-testa_fornecedor()
+fornecedor.testa_funcoes_fornecedor()
 cliente.testa_funcoes_cliente()
 validacao.testa_funcoes_validacao()
 produto.testa_funcoes_produto()
